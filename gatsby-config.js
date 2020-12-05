fba86cbcec3815916e81c4668d85795290b64723
Bob Owen <bob@bobowen.org>
2020-12-05 15:25:32 -0500

    Init gasby project and add GraphCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -10,5 +10,12 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: "gatsby-source-graphcms",
+      options: {
+        endpoint:
+          "https://api-us-east-1.graphcms.com/v2/ckic45swbm40r01xt5c0za1pa/master",
+      },
+    },
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Layout from "components/Layout";
+import Container from "components/Container";
 
-import img_gatsby from 'assets/images/gatsby-astronaut.png';
+import img_gatsby from "assets/images/gatsby-astronaut.png";
 
 const IndexPage = () => {
+  const { graphCmsPage = {} } = useStaticQuery(
+    graphql`
+      query PageQuery {
+        graphCmsPage(id: { eq: "Page:ckic4d7ns1gtg0c30rx31lg8c" }) {
+          tagline
+          headline
+          id
+        }
+      }
+    `
+  );
+  const { tagline, headline } = graphCmsPage;
+
   return (
     <Layout pageName="home">
       <Helmet>
@@ -16,22 +30,8 @@ const IndexPage = () => {
         <p className="gatsby-astronaut">
           <img src={img_gatsby} alt="Build with Gatsby!" />
         </p>
-        <h1>Gatsby Sass Starter</h1>
-        <p>
-          Welcome to your new Gatsby site.
-        </p>
-        <p>
-          Now go build something great.
-        </p>
-        <h2>Still Getting Started?</h2>
-        <p>
-          Run the following in your terminal!
-        </p>
-        <pre>
-          <code>
-            gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-sass
-          </code>
-        </pre>
+        <h1>{headline}</h1>
+        <p>{tagline}</p>
       </Container>
     </Layout>
   );
